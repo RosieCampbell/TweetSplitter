0cf191bdabdd465b757134a88286b1ce1a3ad412
Rosie Campbell <info@rosie-campbell.co.uk>
2013-11-17 01:39:15 +0000

    max check

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 $(function(){
+	var chars;
 	$("#tweet").keyup(function(){
-	  $("#count").text($(this).val().length);
+		chars = $(this).val().length; 
+	  $("#count").text(chars);
+	  checkLength(chars);
 	});
 });
 
+function checkLength(chars){
+	if(chars >= 140){
+		$("#tweets").append("<div class='col-xs-8 column'><textarea id='tweet2' class='form-control' rows='3'></textarea></div>");
+
+	}
+	console.log(chars);
+}
