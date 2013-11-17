9f90e20c598d2e63770ea32825d2a9fe99e2d2d4
Rosie Campbell <info@rosie-campbell.co.uk>
2013-11-17 13:50:26 +0000

    oo refactor

---
+++ TweetBox.js
@@ -0,0 +1,29 @@
+function TweetBox(idNum){
+	var self = this;
+	this.domElement = document.createElement("textarea");
+	this.domElement.id = "tweet";
+	this.domElement.className = "form-control";
+	this.domElement.rows = "3"
+
+	$("#tweets").append("<div id = 'tweetColumn" + idNum + "' class='col-xs-8 column'></div><div class='col-xs-4 column'><div class='row clearfix'><a class='btn btn-primary' id='custom-tweet-button" + idNum + "' href='http://twitter.com/intent/tweet'>Tweet</a></div><div class='row clearfix'><label id='count" + idNum + "' for='tweet'></label></div><script> document.getElementById('custom-tweet-button" + idNum + "').addEventListener('click', function (el) { var tweet = ($('#tweet" + idNum + "').val()); el.target.href += '?text=' +  tweet});</script></div>");
+	document.getElementById("tweetColumn" + idNum + "").appendChild(this.domElement);
+
+	this.charLength = function (){
+		return this.domElement.value.length;
+	}
+	
+	this.domElement.addEventListener("input", onInput, false);
+	this.checkMaxLength = function() {
+		if(self.charLength() === 140){
+			var tweetbox = new TweetBox(idNum+1);
+			tweetbox.domElement.focus();
+		}
+	}
+	function onInput(){
+		document.getElementById('count' + idNum).innerHTML = self.charLength();
+		self.checkMaxLength();
+	}
+	
+	
+}
+
--- script.js
+++ script.js
@@ -1,26 +1,7 @@
+var tweetBoxes = [];
 $(function(){
-	newTextArea(0);
+
+	var tweetBox0 = new TweetBox(0);
+
 });
 
-function checkLength(chars, i){
-	if(chars === 140){
-		newTextArea(i+1);
-	}
-}
-
-function addListeners(i){
-	var t = document.getElementById("tweet"+i);
-	t.addEventListener("input", function(){
-		var chars;
-		chars = $(this).val().length; 
-		$("#count"+i).text(chars);
-		checkLength(chars, i);
-	}, false);
-}
-
-function newTextArea(i){
-	$("#tweets").append("<div class='col-xs-8 column'><textarea id='tweet" + i + "' class='form-control' rows='3'></textarea></div><div class='col-xs-4 column'><label id='count" + i + "' for='tweet" + i + "'></label><a id='custom-tweet-button" + i + "' href='http://twitter.com/intent/tweet'>Tweet</a><script> document.getElementById('custom-tweet-button" + i + "').addEventListener('click', function (el) { var tweet = ($('#tweet" + i + "').val()); el.target.href += '?text=' +  tweet});</script></div>");
-		document.getElementById("tweet" + i).focus();
-		addListeners(i);
-
-}
